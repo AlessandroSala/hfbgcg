9dda00bf1df6141dc3ca3e9303b38d154d951f1f
Alessandro Sala <ale9.sala@gmail.com>
2026-02-13 12:20:20 +0100

    first commit

--- python_utilities/monitor.py
+++ python_utilities/monitor.py
@@ -4,5 +4,5 @@ from pathlib import Path
 for status_file in Path("output").rglob("status.json"):
     with open(status_file) as f:
         s = json.load(f)
-    print(status_file.parent.name, s["iter"], s["converged"])
+    print(status_file.parent.name, s["iter"], s["converged"], s["error"]))
 
